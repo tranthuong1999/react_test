f23911e0835c487865cc5d5268cbf643b80e690c
MartinTran <macpro2015@MartinTran.local>
2025-04-01 23:30:23 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import EmployeeList from './components/EmployeeList';
 
 function App() {
+
+  console.log("chcking",process.env.REACT_APP_BASE_URL)
   return (
     <div className="App">
       <EmployeeList />
--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,7 +1,7 @@
 
 import axios from "axios";
-// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api"
-const BASE_URL = "https://server-test-1-0ilt.onrender.com/api"
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api"
+// const BASE_URL = "https://server-test-1-0ilt.onrender.com/api"
 // Create an Axios instance
 const apiClient = axios.create({
     baseURL: BASE_URL,
